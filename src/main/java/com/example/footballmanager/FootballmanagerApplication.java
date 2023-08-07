ddbba32824993d49b48285b1cee590cb065bf6a8
Nadiia Huryk <nadia11huryk@gmail.com>
2023-08-07 14:59:57 +0300

    first commit

---
+++ src/main/java/com/example/footballmanager/FootballmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballmanagerApplication.class, args);
+    }
+
+}
